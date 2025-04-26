8b91959466a8cbb798980c078ab55d9eab2a265b
Paulo Silva <paulo.silva@conceptu.ind.br>
2025-04-26 13:43:10 -0300

    feat:

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client/extension";
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
