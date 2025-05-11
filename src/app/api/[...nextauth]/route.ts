c092573c82c69a661501484725e90c5d843a9e5f
psilva1982 <severos1982@gmail.com>
2025-05-11 03:47:06 -0300

    init add google account

---
+++ src/app/api/[...nextauth]/route.ts
@@ -0,0 +1,16 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth"
+import { db } from "@/_lib/prisma"
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+})
+
+export { handler as GET, handler as POST }
--- src/components/sidebar-sheet.tsx
+++ src/components/sidebar-sheet.tsx
@@ -1,20 +1,28 @@
-import { quickSearchOptions } from "../_constants/search";
-import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
-import { HomeIcon, CalendarIcon, LogOutIcon, } from "lucide-react";
-import { Button } from "./ui/button";
-import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
-import Image from "next/image";
-import Link from "next/link";
+import { quickSearchOptions } from "../_constants/search"
+import { HomeIcon, CalendarIcon, LogOutIcon, LogInIcon } from "lucide-react"
+import { Button } from "./ui/button"
+import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
+import Image from "next/image"
+import Link from "next/link"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SidebarSheet = () => {
+  return (
+    <SheetContent className="min-h-screen overflow-y-auto">
+      <SheetHeader>
+        <SheetTitle className="text-left">Menu</SheetTitle>
+      </SheetHeader>
 
-    return (
-        <SheetContent className="overflow-y-auto">
-            <SheetHeader>
-                <SheetTitle className="text-left">Menu</SheetTitle>
-            </SheetHeader>
-
-            <div className="flex items-center justify-between gap-3 border-b border-solid ps-4 pb-5">
+      <div className="flex items-center justify-between gap-3 border-b border-solid px-4 pb-5">
+        {/* 
+            
                 <div className="flex items-center gap-2">
                     <Avatar className="size-11">
                         <AvatarImage src="https://avatars.githubusercontent.com/u/18629257?v=4" className="rounded-full"/>
@@ -24,61 +32,74 @@ const SidebarSheet = () => {
                         <p className="text-xs">test@test.com</p>
                     </div>
                 </div>
-            </div>
+                */}
 
-            <div className="flex flex-col gap-2 border-b border-solid pb-5">
-                <SheetClose asChild>
-                    <Button
-                        className="justify-start gap-2"
-                        variant="ghost"
-                        asChild
-                    >
-                        <Link href="/">
-                            <HomeIcon size={18} />
-                            Início
-                        </Link>
-                    </Button>
-                </SheetClose>
-                <Button className="justify-start gap-2" variant="ghost" asChild>
-                    <Link href="/bookings">
-                        <CalendarIcon size={18} />
-                        Agendamentos
-                    </Link>
-                </Button>
-            </div>
+        <h2>Olá, faça seu login</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size={"icon"}>
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando a conta do Google
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant={"outline"} className="gap-1 font-bold">
+              <Image src="/google.svg" width={18} height={18} alt="" />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+      </div>
 
-            <div className="flex flex-col gap-2 border-b border-solid pb-5">
-                {quickSearchOptions.map((option) => (
-                    <SheetClose key={option.title} asChild>
-                        <Button
-                            className="justify-start gap-2"
-                            variant="ghost"
-                            asChild
-                        >
-                            <Link href={`/barbershops?service=${option.title}`}>
-                                <Image
-                                    alt={option.title}
-                                    src={option.imageUrl}
-                                    height={18}
-                                    width={18}
-                                    aria-description={option.title}
-                                />
-                                {option.title}
-                            </Link>
-                        </Button>
-                    </SheetClose>
-                ))}
-            </div>
+      <div className="flex flex-col gap-2 border-b border-solid pb-5">
+        <SheetClose asChild>
+          <Button className="justify-start gap-2" variant="ghost" asChild>
+            <Link href="/">
+              <HomeIcon size={18} />
+              Início
+            </Link>
+          </Button>
+        </SheetClose>
+        <Button className="justify-start gap-2" variant="ghost" asChild>
+          <Link href="/bookings">
+            <CalendarIcon size={18} />
+            Agendamentos
+          </Link>
+        </Button>
+      </div>
 
-            <div className="flex flex-col gap-2">
-                <Button variant="ghost" className="justify-start gap-2">
-                    <LogOutIcon size={18} />
-                    Sair da conta
-                </Button>
-            </div>
+      <div className="flex flex-col gap-2 border-b border-solid pb-5">
+        {quickSearchOptions.map((option) => (
+          <SheetClose key={option.title} asChild>
+            <Button className="justify-start gap-2" variant="ghost" asChild>
+              <Link href={`/barbershops?service=${option.title}`}>
+                <Image
+                  alt={option.title}
+                  src={option.imageUrl}
+                  height={18}
+                  width={18}
+                  aria-description={option.title}
+                />
+                {option.title}
+              </Link>
+            </Button>
+          </SheetClose>
+        ))}
+      </div>
 
-        </SheetContent>
-    );
+      <div className="mt-auto mb-2 flex flex-col gap-2">
+        <Button variant="ghost" className="justify-start gap-2">
+          <LogOutIcon size={18} />
+          Sair da conta
+        </Button>
+      </div>
+    </SheetContent>
+  )
 }
 
-export default SidebarSheet;
+export default SidebarSheet
